71fd7a4816ae9c1db95ffd0ceee5626a525d38e2
Carlos Chao <chao@yareytech.com>
2025-03-13 13:25:58 -0600

    feat(fe): create dappas web app with next and jest for unit test
    re #9

---
+++ apps/dappas-web/jest.config.ts
@@ -0,0 +1,11 @@
+export default {
+  displayName: 'dappas-web',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/next/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/dappas-web',
+  testEnvironment: 'jsdom',
+};

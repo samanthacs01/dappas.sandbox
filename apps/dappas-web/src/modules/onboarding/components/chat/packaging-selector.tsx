71391ac48f1e150c86c30caf1360784681907545
isabella.gl <isabella.gl@avangenio.com>
2025-05-01 14:05:43 -0400

    refactor: display the component in the center of the message

--- apps/dappas-web/src/modules/onboarding/components/chat/packaging-selector.tsx
+++ apps/dappas-web/src/modules/onboarding/components/chat/packaging-selector.tsx
@@ -34,22 +34,24 @@ const PackagingSelector: React.FC<PackagingSelectorProps> = ({
   };
 
   return (
-    <div className="grid grid-cols-5 w-[300px] gap-2">
-      {packageIcons.map((packageIcon) => (
-        <div
-          key={packageIcon.id}
-          className={clsx(
-            'flex items-center justify-center p-3 cursor-pointer transition-colors duration-200 rounded-md border hover:border-orange-500 hover:text-orange-500'
-          )}
-          onClick={() => handlePackageClick(packageIcon.id)}
-        >
-          <packageIcon.icon
-            className={clsx('size-6', {
-              'text-orange-500': selectedIcon === packageIcon.id,
-            })}
-          />
-        </div>
-      ))}
+    <div className="flex w-full justify-center pt-2">
+      <div className="grid grid-cols-5 w-[300px] gap-2">
+        {packageIcons.map((packageIcon) => (
+          <div
+            key={packageIcon.id}
+            className={clsx(
+              'flex items-center justify-center p-3 cursor-pointer transition-colors duration-200 rounded-md border hover:border-orange-500 hover:text-orange-500'
+            )}
+            onClick={() => handlePackageClick(packageIcon.id)}
+          >
+            <packageIcon.icon
+              className={clsx('size-6', {
+                'text-orange-500': selectedIcon === packageIcon.id,
+              })}
+            />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };

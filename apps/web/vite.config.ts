87aa189d1bfde47a91258ede191fde5c0024b640
Carlos Chao <chao@yareytech.com>
2025-05-15 10:20:25 -0400

    feat: create dockerfile configuration for react app

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,14 +1,25 @@
-import tailwindcss from "@tailwindcss/vite"
-import react from "@vitejs/plugin-react"
-import path from "path"
-import { defineConfig } from "vite"
- 
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { defineConfig } from 'vite';
+
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
-})
+  preview: {
+    port: 8080,
+    strictPort: true,
+  },
+  server: {
+    port: 8080,
+    strictPort: true,
+    host: true,
+    origin: 'http://0.0.0.0:8080',
+  },
+});
